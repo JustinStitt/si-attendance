cb8097404d9503bf563835875fb482574b4f7710
Justin Stitt <cpt.crixus@gmail.com>
2022-08-27 10:31:18 -0700

    cors patch

--- src/index.py
+++ src/index.py
@@ -1,8 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from attendance import Attendance
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"*": {"origins": "*"}})
 api = Api(app)
 
 
@@ -12,12 +14,15 @@ class HelloWorld(Resource):
 
 
 class SignIn(Resource):
-    def post(self):
+    def get(self):
         args = request.args
-        print(args)
+        response = jsonify({"message": "request made"})
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        if not len(args):
+            return response
         bot = Attendance()
-        response = bot.signIn(cwid=args["cwid"], course=args["course"])
-        return {"course_selection_status_code": response}
+        bot.signIn(cwid=args["cwid"], course=args["course"])
+        return response
 
 
 api.add_resource(SignIn, "/signin")
